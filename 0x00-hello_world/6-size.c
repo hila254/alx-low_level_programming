f7fbb49171e596afe6ccc22ab28273dfe7474580
hila254 <khilary4400@gmail.com>
2023-03-18 11:54:07 -0700

    6. Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,25 @@
  */
 int main(void)
 {
-char u;
-int v;
-long int w;
-long long int x;
-float y;
-printf("Size of char: %zd byte(s)\n", sizeof(u));
-printf("Size of int: %zd byte(s)\n", sizeof(v));
-printf("Size of long int: %zd byte(s)\n", sizeof(w));
-printf("Size of long long int: %zd byte(s)\n", sizeof(x));
-printf("Size of float: %zd byte(s)\n", sizeof(y));
-return (0);
+	char u;
+	int v;
+	long int w;
+	long long int x;
+	float y;
+
+	printf("Size of char: %zd byte(s)\n", sizeof(u));
+	printf("Size of int: %zd byte(s)\n", sizeof(v));
+	printf("Size of long int: %zd byte(s)\n", sizeof(w));
+	printf("Size of long long int: %zd byte(s)\n", sizeof(x));
+	printf("Size of float: %zd byte(s)\n", sizeof(y));
+
+#if defined(__x86_64__) || defined(__ppc64__)
+	/* Output for 64-bit systems */
+	printf("This is a 64-bit system.\n");
+#else
+	/* Output for 32-bit systems */
+	printf("This is a 32-bit system.\n");
+#endif
+
+	return (0);
 }
