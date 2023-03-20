9c163d71f774decaa94810d19d9f242fbc4c9103
hila254 <khilary4400@gmail.com>
2023-03-19 22:12:57 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,11 +10,13 @@ int main(void)
 	/* Initialize a variable to store the current letter to be printed.*/
 	int numb;
 
-	/* Loop through numbers 0 to 10 and print each on a new line */
-	for (numb = 0; numb < 11; ++numb)
+	/* Loop through numbers 0 to 9 and print each on a new line */
+	for (numb = 0; numb < 10; ++numb)
 	{
 		/* Print current number */
 		printf("%d", numb);
 	}
+	printf("\n");
+
 	return (0);
 }
